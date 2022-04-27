faa38173ad3fece02ad25509c9ec79923003da22
hizamianuar <hizamianuar66@gmail.com>
2022-04-27 10:39:32 -0400

    added basic general derivation handler

--- backbone.scm
+++ backbone.scm
@@ -1,4 +1,4 @@
-;;(load "logic.scm")
+(load "logic.scm")
 
 (define (define-rule
 	  name
@@ -60,17 +60,40 @@
        (not1? (second psi))
        (not1? (third psi))))
 
-(define (derive-unary
-	 procedure
-	 applicability
-	 derived
-	 sentences)
-  (let ((possibilities (filter (lambda (sentence)
-				 (applicability (car sentence)))
-			       sentences)))
-    (find-first (lambda (sentence)
-		(procedure derived (car sentence) (cdr sentence)))
-		possibilities)))
+(define (get-sentence sentence) (first sentence))
+(define (get-premise-set sentence) (second sentence))
+
+(define (derive-recursive procedure
+			  reversed-applicability
+			  derived
+			  sentences			  
+			  args)
+  (if (null? reversed-applicability)
+      (apply procedure (cons derived args))
+      (let ((possibilities (filter (lambda (sentence)
+				     ((car reversed-applicability)
+				      (get-sentence sentence)))
+				   sentences)))
+	(find-first (lambda (sentence)
+		      (derive-recursive procedure
+					(cdr reversed-applicability)
+					derived
+					sentences
+					(cons (get-sentence sentence)
+					      (cons (get-premise-set sentence)
+						    args))))
+		    possibilities))))
+
+(define (derive-general procedure
+			applicability
+			derived
+			sentences)
+  (derive-recursive procedure
+		    (reverse applicability)
+		    derived
+		    sentences
+		    (list)))
+			
 
 ;;; Finds the first element of lis where pred is not #f
 ;;; Returns the element and the result of pred
@@ -92,7 +115,71 @@
 		      '(c y)))
   (define delta (cons '(not (and a b))
 		      '(d z)))
-  (define res (derive-unary demorgans demorgans? derived (list alpha beta gamma)))
-  (write (car res)) (newline)
-  (write (cdr res)) (newline)
+  (define res (derive-general demorgans
+			      (list demorgans?)
+			      derived
+			      (list alpha beta gamma)))
+  (write res)
+  ;(write (car res)) (newline)
+  ;(write (cdr res)) (newline)
 )
+
+
+(begin
+  (define derived '(implies  (and a b)))
+  (define alpha (cons '(not (and (not a) (not b)))
+		      '(a w)))
+  (define beta (cons '(or a b)
+		     '(b x)))
+  (define gamma (cons '(or (not a) (not b))
+		      '(c y)))
+  (define delta (cons '(not (and a b))
+		      '(d z)))
+  (define res (derive-general demorgans
+			      (list demorgans?)
+			      derived
+			      (list alpha beta gamma)))
+  (write res)
+  ;(write (car res)) (newline)
+  ;(write (cdr res)) (newline)
+  )
+(define der '(implies ((atomic a) (constant s)) ((atomic b) (constant s))))
+(define psi '((atomic b) (constant s)))
+(define phi '((atomic a) (constant s)))
+(define premise-set (list phi))
+
+
+(define (modus-ponens
+	 derived
+	 phi
+	 phi-premise-set
+	 psi
+	 psi-premise-set)
+  (and
+   (implies? phi)
+   (equal? psi (second phi))
+   (equal? derived (third phi))
+   (lset-union equal? phi-premise-set psi-premise-set)))
+
+(define true? (lambda (x) #t))
+
+(define (implies? exp)
+  (and (list? exp)
+       (= (length exp) 3)
+       (eqv? (tag exp) 'implies)))
+
+(derive-general modus-ponens
+		(list implies? true?)
+		'a
+		(list (list '(implies b a)
+			    '(p x))
+		      (list 'c
+			    '(q x))
+		      (list 'd
+			    '(r x))
+		      (list '(implies c a)
+			    '(s x))
+		      (list '(implies a a)
+			    '(t x))))
+;; premise sentences - ((c (q x)) ((implies c a) (s x))
+;; premise set - q s x)
