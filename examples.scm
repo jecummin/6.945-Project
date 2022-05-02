a63355d955e619b2d5a7141e89fd54048b2b0526
Jesse E Cummings <jecummin@csail.mit.edu>
2022-05-02 13:36:39 -0400

    Add new test case

--- examples.scm
+++ examples.scm
@@ -14,6 +14,34 @@
 	'(implies ((atomic m) (constant s)) ((atomic d) (constant s)))
 	'((atomic d) (constant s))))
 
-(define result (analyze-argument socrates-is-mortal-argument))
-(describe-argument result)
+ ;; (define result (analyze-argument socrates-is-mortal-argument))
+ ;; (describe-argument result)
+
+
+;; f -> is a fish
+;; w -> is warm-blooded
+;; a -> is aquatic animal
+(define not-all-aquatic-animals-are-fish
+  (list '(lnot (existential (variable x)
+			    (land ((atomic f) (variable x))
+				  ((atomic w) (variable x)))))
+	'(existential (variable x)
+		      (land ((atomic a) (variable x))
+			    ((atomic w) (variable x))))
+	'(land ((atomic a) (constant s)) ((atomic w) (constant s)))
+	'(universal (variable x)
+		    (lnot (land ((atomic f) (variable x))
+				((atomic w) (variable x)))))
+	'(lnot (land ((atomic f) (constant s))
+		     ((atomic w) (constant s))))
+	'((atomic w) (constant s))
+	'(lnot ((atomic f) (constant s)))
+	'((atomic a) (constant s))
+	'(land ((atomic a) (constant s)) ((atomic f) (constant s)))
+	'(existential (variable x) (land ((atomic a) (variable x))
+					 ((atomic f) (constant s))))
+	'(existential (variable x) (land ((atomic a) (variable x))
+					 ((atomic f) (constant s))))))
 			     
+(define result (analyze-argument not-all-aquatic-animals-are-fish))
+(describe-argument result)
