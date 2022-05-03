4dbd5671a8826db346eb50e4a1e2563c176b62f5
hizamianuar <hizamianuar66@gmail.com>
2022-05-03 09:31:09 -0400

    skeleton for auto-derivation

--- backbone.scm
+++ backbone.scm
@@ -89,7 +89,6 @@
     (if (null? reversed-applicability)
 	(let ((res (apply procedure (cons derived args))))
 	  (and res
-	       (write "result: ") (write res) (newline)
 	       (not (member (list derived res) sentences))
 	       (cons (list) res)))
 	(let ((possibilities (filter (lambda (sentence)
@@ -181,3 +180,86 @@
 
   (describe-argument res)
 )
+
+;;; Returns false if rule is not applicable
+;;; Otherwise returns pair
+;;; car is the list of premise sentences
+;;; cdr is a list containing derived and premise set
+(define (try-autoderive procedure
+		        applicability
+		        sentences)
+  (let lp ((procedure procedure)
+	   (reversed-applicability (reverse applicability))
+	   (sentences sentences)
+	   (args (list)))			   
+    (if (null? reversed-applicability)
+	(let ((derived (apply procedure args)))
+	  (and derived
+	       (not (member derived sentences))
+	       (cons (list) derived)))
+	(let ((possibilities (filter (lambda (sentence)
+				       ((car reversed-applicability)
+					(get-sentence sentence)))
+				     sentences)))
+	  (let ((res (find-first (lambda (sentence)
+				   (lp procedure
+				       (cdr reversed-applicability)
+				       sentences
+				       (cons sentence args)))
+				 possibilities)))
+	    (if res
+		(cons (cons (car res)
+			    (car (cdr res)))
+		      (cdr (cdr res)))
+		#f))))))
+
+(define (autoderive-rules rules
+			  ;target
+			  derivations)
+  (let ((sentences-and-premises
+	 (map (lambda (derivation)
+		(list (derivation-sentence derivation)
+		      (derivation-premise-set derivation)))
+	      derivations)))
+    (let ((deduction
+	   (find-first (lambda (rule)
+			 (try-autoderive (rule-procedure rule)
+				    (rule-applicability rule)
+				    sentences-and-premises))
+		       rules)))
+      (if deduction
+	  (list (second (cdr deduction))
+		(third (cdr deduction))
+		(rule-name (car deduction))
+		(first (cdr deduction)))
+	  #f))))
+
+(define (derive-modus-ponens phi-and-premise-set
+			     psi-and-premise-set)
+  (define phi (get-sentence phi-and-premise-set))
+  (define phi-premise-set (get-premise-set phi-and-premise-set))
+  (define psi (get-sentence psi-and-premise-set))
+  (define psi-premise-set (get-premise-set psi-and-premise-set))
+  (and
+   (implies? phi)
+   (equal? psi (second phi))
+   (let ((derived (third phi))
+	 (premise-set (lset-union equal?
+				  phi-premise-set
+				  psi-premise-set)))
+     (list derived premise-set))))
+
+(define modus-ponens-rule (list 'modus-ponens
+			        derive-modus-ponens
+				(list implies? true?)))
+
+(let ((step (autoderive-rules (list modus-ponens-rule)
+	    '(((implies phi psi)
+	       ((implies phi psi))
+	       premise-induction
+	       ((implies phi psi)))
+	      (phi
+	       (phi)
+	       premise-induction
+	       (phi))))))
+  (describe-step step))
